613c8c8e2db00b640c6dba67c517ca5d0fcb7a2f
huangjie <huangjie@youzan.com>
2018-11-21 18:30:00 +0800

    配置项目开发环境

---
+++ build/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  HTMLDirs: [
+    {
+      page: 'index',
+      title: 'hello'
+    },
+    {
+      page: 'chat',
+      title: 'secret-chat',
+    }
+  ],
+  cssPublicPath: '../',
+  imgOutputPath: 'img/',
+  fontOutputPath: 'font',
+  cssOutputPath: './css/styles.css',
+  devServerOutputPath: '../dist',
+};
